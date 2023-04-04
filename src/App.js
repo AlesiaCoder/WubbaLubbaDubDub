b3e6f25039dc7a419d69cc871a2be435e40b2bd4
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-04-04 21:40:46 +0200

    T04 E08 · The Vat of Acid Episode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 /*import logo from './logo.svg';*/
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AllCharacters from './components/AllCharacters';
+import OneCharacter from './components/OneCharacter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <img src=''/*{RickAndMortyLogo}*/ alt='Logotipo Rick and Morty' className='logo'></img>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AllCharacters></AllCharacters>}></Route>
+          <Route path='/character/:id' element={<OneCharacter></OneCharacter>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
